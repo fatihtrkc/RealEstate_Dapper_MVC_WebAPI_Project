6bf01b4a10bd40a93e00bbb28fb4b301692aa32c
Fatih Tarakcı <fatih_trkci96@hotmail.com>
2023-08-05 21:25:13 +0300

    Add project files.

---
+++ RealEstate_Dapper_MVC/Controllers/CustomerLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_MVC.Controllers
+{
+    public class CustomerLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
